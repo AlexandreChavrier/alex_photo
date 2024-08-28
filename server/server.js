faadd9f7d5e717f45a9d7e2533f8623f5beaa22c
alexandre chavrier <alex69.chavrier@gmail.com>
2024-08-28 12:13:00 +0200

    config server et client vite react node

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+    res.json({ fruits: ["apple", "strawberry", "pineapple"] })
+});
+
+app.listen(8080, () => {
+    console.log("server started on port 8080");
+});
+
